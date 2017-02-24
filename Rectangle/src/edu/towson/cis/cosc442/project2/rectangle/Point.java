53d658b3a8665c4fe2ee386f64ce068c48948102
b3bry <bateoa1@students.towson.edu>
2017-02-23 20:27:29 -0500

    Changed Point's x and y variable to be private and accessible through getters and setters. This allows future faults with these variables to be found easier with simple unit tests.

--- Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/Point.java
+++ Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/Point.java
@@ -6,7 +6,7 @@ package edu.towson.cis.cosc442.project2.rectangle;
 public class Point {
 	
 	/** x and y coordinates. */
-	public Double x, y;
+	private Double x, y;
 	
 	/**
 	 * Instantiates a new point.
@@ -15,7 +15,23 @@ public class Point {
 	 * @param y the y
 	 */
 	Point(Double x, Double y) {
-		this.x = y;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Double getX(){
+		return x;
+	}
+	
+	public void setX(Double x){
+		this.x = x;
+	}
+	
+	public Double getY(){
+		return y;
+	}
+	
+	public void setY(Double y){
 		this.y = y;
 	}
 }
